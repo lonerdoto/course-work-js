b9e2019a06d69f753fc5ff7d76dc3a44fb128f2f
lonerdoto <kotlirov530@mail.ru>
2023-12-04 02:18:58 +0300

    some fix

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,16 +1,19 @@
 import { renderCards } from "./modules/cards.js";
 import { redirectLogo } from "./modules/logo-redirect.js";
 import { auth, controlModal } from "./modules/modal.js";
-import { getData } from "./modules/getServerData.js";
+import { deleteGoods, getData } from "./modules/getServerData.js";
 import {renderItems} from "../scripts/modules/render.js";
-import { formControl} from "./modules/starting.js";
+import {popupControl, formControl, deleteFunction, editItemsFunction} from "./modules/starting.js";
 import {filter} from "../scripts/modules/filter.js";
 
+let addProductBtn = document.querySelector(".add-product-btn");
 let popup = document.querySelector(".modal-edit");
+let closeBtn = document.querySelector(".pop-up__close");
 const form = document.querySelector(".form ");
 const data = await getData();
 
 const init = async () => {
+	popupControl(addProductBtn, closeBtn, popup);
 	await renderItems();
 	formControl(form, data);
 	filter();
